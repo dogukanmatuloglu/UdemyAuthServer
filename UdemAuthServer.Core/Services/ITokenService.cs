46b371af8b35055834fd83a0df0e11db79a4f1a3
Dogukan Matuloglu <dogukan.matul@gmail.com>
2022-09-24 22:53:29 +0300

    ITokenService interfaci oluşturuldu

---
+++ UdemAuthServer.Core/Services/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemAuthServer.Core.Configuration;
+using UdemAuthServer.Core.Dtos;
+using UdemAuthServer.Core.Models;
+
+namespace UdemAuthServer.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+        ClientTokenDto CreateTokenByClient(Client client)
+    }
+}
