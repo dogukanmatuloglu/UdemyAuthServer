fae0e344e5366cac94f6a0bc17b099aa9f9de10a
Dogukan Matuloglu <dogukan.matul@gmail.com>
2022-09-24 21:29:11 +0300

    TokenDto ve TokenCLientDto sınıfları oluşturuldu

---
+++ UdemAuthServer.Core/Dtos/ClientTokenDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemAuthServer.Core.Dtos
+{
+    public class ClientTokenDto
+    {
+        public string AccesToken { get; set; }
+        public DateTime AccesTokenExpiration { get; set; }
+    }
+}
