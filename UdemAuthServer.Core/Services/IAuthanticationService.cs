ec8dac41d92f68fb2d368f28da5fdef48e7a9916
Dogukan Matuloglu <dogukan.matul@gmail.com>
2022-09-24 23:09:01 +0300

    ITokenService Interfaci oluşturuldu

---
+++ UdemAuthServer.Core/Services/IAuthanticationService.cs
@@ -0,0 +1,18 @@
+﻿using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemAuthServer.Core.Dtos;
+
+namespace UdemAuthServer.Core.Services
+{
+    public interface IAuthanticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+        Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+    }
+}
