1822800d551b5a14b9f7b9abf4f605789a61052e
Dogukan Matuloglu <dogukan.matul@gmail.com>
2022-09-26 20:57:18 +0300

    UnitOfWork sınıfı oluşturuldu

---
+++ UdemyAuthServer.Data/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemAuthServer.Core.UnitOfWork;
+
+namespace UdemyAuthServer.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
