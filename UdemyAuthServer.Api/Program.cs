a9e28b0dc55a79fa3e3d96657be7316437567d0a
Dogukan Matuloglu <dogukan.matul@gmail.com>
2022-09-28 00:24:53 +0300

    Options Pattern ile CustomTokenOptions sınıfı bind edildi

--- UdemyAuthServer.Api/Program.cs
+++ UdemyAuthServer.Api/Program.cs
@@ -1,5 +1,8 @@
+using SharedLibrary.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+builder.Services.Configure<CustomTokenOptions>(builder.Configuration.GetSection("TokenOptions");
 
 app.MapGet("/", () => "Hello World!");
 
