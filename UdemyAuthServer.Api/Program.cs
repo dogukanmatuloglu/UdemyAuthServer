ee5c8793926e13d4250a91a16840de66d9a574d1
Dogukan Matuloglu <dogukan.matul@gmail.com>
2022-10-03 16:48:28 +0300

    User Service Sınıfı tamamlandı

--- UdemyAuthServer.Api/Program.cs
+++ UdemyAuthServer.Api/Program.cs
@@ -1,9 +1,13 @@
 using SharedLibrary.Configuration;
 using UdemAuthServer.Core.Configuration;
+using UdemAuthServer.Core.Services;
+using UdemyAuthServer.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<CustomTokenOptions>(builder.Configuration.GetSection("TokenOptions"));
 builder.Services.Configure<List<Client>>(builder.Configuration.GetSection("Clients"));
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
